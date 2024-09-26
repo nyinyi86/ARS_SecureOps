3f51eafcefa316df234cc94161179f88dec3e38d
nyinyi86 <nyinyiaung.nov@gmail.com>
2024-09-26 14:08:02 +0000

    add 4. create-tfc-dynamic-provider

---
+++ 4.create-tfc-dynamic-provider-credentials/output.tf
@@ -0,0 +1,9 @@
+output "bound_claims" {
+    description = "Vault JWT Auth Backend Role's bound_claim"
+    value =  vault_jwt_auth_backend_role.jwt_role.bound_claims  
+}
+
+output "role_name" {
+    description = "Vault JWT Auth Backend Role's role_name"
+    value =  vault_jwt_auth_backend_role.example.role_name
+}
---
+++ 4.create-tfc-dynamic-provider-credentials/version.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    vault = {
+      source = "hashicorp/vault"
+      version = "4.4.0"
+    }
+  }
+}
+
+provider "vault" {
+  # Configuration options
+}
+
